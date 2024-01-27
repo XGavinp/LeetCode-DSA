6a76e0df6887386c6da5267363000b2a249f9d56
XGavinp <gavinpereira2901@gmail.com>
2024-01-27 17:51:47 +0530

    new file:   TypeScript/Left and Right Sum Differences.ts

---
+++ TypeScript/Left and Right Sum Differences.ts
@@ -0,0 +1,12 @@
+function leftRightDifference(nums: number[]): number[] {
+    let left = 0,
+        right = nums.reduce((a, b) => a + b);
+    const ans: number[] = [];
+    for (const x of nums) {
+        right -= x;
+        ans.push(Math.abs(left - right));
+        left += x;
+    }
+    return ans;
+
+};
